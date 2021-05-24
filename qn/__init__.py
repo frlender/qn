da764f8656bdb349b700ba7e3f2a5c6fa9045abf
duanqiaonan <duanqiaonan@wz.hpc.org>
2021-05-24 10:07:50 +0800

    add qn.config. put viz function in separate submodule.

--- qn/__init__.py
+++ qn/__init__.py
@@ -1,1 +1,5 @@
 from .qn import *
+from .diskIO import *
+# put files in __all__ variable, then you can do import qn.viz as viz or from qn import viz
+# in the package import command
+__all__=['viz']
--- qn/qn.py
+++ qn/qn.py
@@ -4,18 +4,37 @@ try:
 	from scipy.misc import comb
 except:
 	from scipy.special import comb
-from sklearn.decomposition import PCA
-from sklearn.manifold import MDS
-from mpl_toolkits.mplot3d import Axes3D
-import matplotlib.pyplot as plt
+
 from collections import OrderedDict
 import numpy as np
 from scipy.stats import rankdata
 import pandas as pd
+import os
 
-from .diskIO import *
+# from .diskIO import *
 
+def setdir(path):
+    try:
+        os.chdir(path)
+    except:
+        pass
+    return 
 
+def env(env_name):
+    return os.getenv(env_name)
+
+def config(glb,mode='default'):
+    config_path = env('PY_CONFIGS')
+    mode_path = config_path + '/' + mode + '.py'
+    code = load(mode_path,'txt')
+    # exec(code,global())
+    # return _q
+    ns = {} # use ns to prevent contaminating the glb namespace
+    exec(code,ns)
+    glb['_q'] = ns['_q']
+    return 0
+    
+    
 def pinsplit(string,pattern,rangeValue):
 	# split a string by pattern at designated point
 	# e.g. pinsplit('a,b,ccd-5,6',',',3) outputs ['a,b,ccd-5','6']
@@ -75,7 +94,7 @@ def flatList(listOfLists):
 def printEvery(unit,n,string=None):
 	if n%unit==0:
 		if string:
-			print(string)
+			print(string.format(n))
 		else:
 			print(n)
 
@@ -86,59 +105,6 @@ def csv2list(pathx,delim='\t'):
             line = line.strip('\r\n\t')
             res.append(line.split(delim))
     return res
-
-def plotPCA(mat,labels):
-    # columns are samples and rows are genes
-    pca = PCA(n_components=3)
-    pca.fit(mat)
-    ax = scatter3(pca.components_.T,labels)
-    ax.set_xlabel('PC1 ('+format(pca.explained_variance_ratio_[0],'.2')+')')
-    ax.set_ylabel('PC2 ('+format(pca.explained_variance_ratio_[1],'.2')+')')
-    ax.set_zlabel('PC3 ('+format(pca.explained_variance_ratio_[2],'.2')+')')
-
-def plotMDS(distMat,groups=None,labels=None):
-	# input should be distance matrix
-	mds = MDS(dissimilarity="precomputed",
-	max_iter=10000,eps=1e-6)
-	mds.fit(distMat)
-	coordinates = mds.embedding_
-	fig = plt.figure()
-	ax = fig.add_subplot(111)
-	if groups:
-		colors = ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf"]
-		uniqGroups = list(set(groups))
-		groupColors = []
-		for group in groups:
-			idx = uniqGroups.index(group)
-			groupColors.append(colors[idx])
-		ax.scatter(coordinates[:,0],coordinates[:,1],c=groupColors)
-	else:
-		ax.scatter(coordinates[:,0],coordinates[:,1])
-	if labels:
-		for i,label in enumerate(labels):
-			ax.annotate(label,xy=(coordinates[i,0],coordinates[i,1]),
-			xytext=(coordinates[i,0],coordinates[i,1]+0.05))
-	return ax
-
-
-def scatter3(mat,labels):
-    colors = ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf"]
-    # colors = ['r','b','g','c','m','y',]
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    plottedLabels = []
-    for i, label in enumerate(set(labels)):
-        plottedLabels.append(label)
-        idx = np.array([True if item==label else False for item in labels])
-        print(label,colors[i])
-        subMat = mat[idx,:]
-        xs = subMat[:,0]
-        ys = subMat[:,1]
-        zs = subMat[:,2]
-        ax.scatter(xs, ys, zs, c=colors[i], marker='o')
-    plt.show()
-    plt.legend(plottedLabels)
-    return ax
 
 def tidy_split(df, column, sep='|', keep=False):
     """
